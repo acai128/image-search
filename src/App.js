043cbb5f75bfb79a32e70442e5df9569c3027988
Amy Cai <amycai89@gmail.com>
2021-01-11 11:29:18 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'; 
+import SearchForm from './SearchForm'; 
+import SearchResults from './SearchResults'
+import NavBar from "./NavBar"; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <SearchForm />
+      {/* <SearchResults /> */}
     </div>
   );
 }
---
+++ src/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css'; 
+
+function NavBar() {
+    return (
+    
+            <h1 class="title">Image Search</h1>
+       
+    )
+}
+
+export default NavBar; 
